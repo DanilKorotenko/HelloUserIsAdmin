93f32c773c6f8d4333a727881f035ffae74d5abd
Danil Korotenko <danil.korotenko@gmail.com>
2021-06-03 17:23:03 +0300

    Initial implementation

--- HelloUserIsAdmin/main.m
+++ HelloUserIsAdmin/main.m
@@ -6,11 +6,72 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <Collaboration/Collaboration.h>
 
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        // insert code here...
-        NSLog(@"Hello, World!");
+BOOL isUserAdmin(NSString *aUserName);
+gid_t getAdminIdentityGroupID(void);
+
+
+gid_t getAdminIdentityGroupID()
+{
+    CSIdentityQueryRef groupQuery = CSIdentityQueryCreateForName(
+        kCFAllocatorDefault, CFSTR("admin"), kCSIdentityQueryStringEquals,
+        kCSIdentityClassGroup, CSGetLocalIdentityAuthority());
+
+    CSIdentityQueryExecute(groupQuery, kCSIdentityQueryIncludeHiddenIdentities, NULL);
+    CFArrayRef groupQueryResults = CSIdentityQueryCopyResults(groupQuery);
+
+    gid_t adminGroupId = 0;
+
+    if (groupQueryResults)
+    {
+        CFIndex resultsCount = CFArrayGetCount(groupQueryResults);
+
+        if (resultsCount == 1)
+        {
+            CSIdentityRef adminGroupIdentity =
+                (CSIdentityRef)CFArrayGetValueAtIndex(groupQueryResults, 0);
+            adminGroupId = CSIdentityGetPosixID(adminGroupIdentity);
+        }
+
+        CFRelease(groupQueryResults);
     }
-    return 0;
+
+    return adminGroupId;
+}
+
+BOOL isUserAdmin(NSString *aUserName)
+{
+    CBIdentity *userIdentity = [CBIdentity identityWithName:aUserName
+        authority:[CBIdentityAuthority defaultIdentityAuthority]];
+
+    CBGroupIdentity *groupIdentity = [CBGroupIdentity
+        groupIdentityWithPosixGID:getAdminIdentityGroupID()
+        authority:[CBIdentityAuthority localIdentityAuthority]];
+
+    BOOL isAdmin = NO;
+
+    if (groupIdentity != nil)
+    {
+        // check if the user is currently a member of the admin group
+        isAdmin = [userIdentity isMemberOfGroup:groupIdentity];
+    }
+
+    return isAdmin;
+}
+
+int main(int argc, const char * argv[])
+{
+    BOOL isAdmin = NO;
+
+    @autoreleasepool
+    {
+        NSString *userName = NSUserName();
+        isAdmin = isUserAdmin(userName);
+        NSString *userRole = (isAdmin ? @"Admin" : @"Standart User");
+
+        NSLog(@"User: %@ is %@", userName, userRole);
+    }
+
+    return (isAdmin ? 0 : 1);
 }
